20828ffd35c7ab29e67b4b7164d85cffeadad3b9
Stephan Bönnemann <stephan@boennemann.me>
2014-04-25 15:52:29 +0200

    feat(sorttasks): put taskSort logic into service

---
+++ app/scripts/services/sorttasks.js
@@ -0,0 +1,84 @@
+angular.module('zentodone').service('sortTasks', function ($filter) {
+  return function(collection, unit) {
+    var sortedTasks = [];
+    var rescheduledTasks = [];
+    var today = new Date();
+
+    function unitsOff(date) {
+      date = new Date(date)
+      return Math.round((date.getTime() - today.getTime())/(unit))
+    }
+
+    function reschedule(task) {
+      delete task.dueDate
+      rescheduledTasks.push(task)
+    }
+
+    collection = $filter('orderBy')(collection, function(task) {
+      return task.dueDate
+    })
+
+    // Fill up units w/tasks
+    for (var i = 0; i < collection.length; i++) {
+      var currentTask = collection[i]
+
+      // Tasks w/o dueDate have lowest priority and are therefore rescheduled
+      if (!currentTask.dueDate) {
+        reschedule(currentTask)
+        continue
+      }
+
+      var unitOffset = unitsOff(currentTask.dueDate)
+
+      // Overdue tasks are inherited for this unit with highest priority
+      if (unitOffset < 0) {
+        var overdue = Math.abs(unitOffset)
+        currentTask.dueDate += (overdue * unit)
+
+        if (!angular.isNumber(currentTask.overdue)) {
+          currentTask.overdue = 0
+        }
+
+        currentTask.overdue += overdue
+        unitOffset = 0
+      }
+
+      var currentUnit = sortedTasks[unitOffset]
+
+      if (!angular.isArray(currentUnit)) {
+        currentUnit = sortedTasks[unitOffset] = []
+      }
+
+      // Its not allowed to have more than 3 Tasks per unit
+      if (currentUnit.length > 2) {
+        reschedule(currentTask)
+        continue
+      }
+
+      currentUnit.push(currentTask)
+    }
+
+    // Rescheduled Tasks are reordered, so that the oldest Tasks have to be done first
+    rescheduledTasks = $filter('orderBy')(rescheduledTasks, function(task) {
+      return task.date
+    })
+
+    // Fill up units w/rescheduled taskss
+    for (var j = 0; j < rescheduledTasks.length; j++) {
+      var unitIndex = sortedTasks.length-1
+      var unitToFill = sortedTasks[unitIndex]
+
+      // Either there's space remaining in an existing unit or a new unit is initialized
+      if (angular.isUndefined(unitToFill) || unitToFill.length > 2) {
+        unitToFill = []
+        unitIndex = sortedTasks.push(unitToFill) - 1
+      }
+
+      var taskToAssign = rescheduledTasks[j]
+      taskToAssign.dueDate = today.getTime() + (unitIndex * unit)
+      unitToFill.push(taskToAssign)
+    }
+
+    return sortedTasks
+  }
+});
